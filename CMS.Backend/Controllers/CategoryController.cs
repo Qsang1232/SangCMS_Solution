a7d82c584b895e86dee047e17996776c6844152e
Qsang1232 <nqs201105@gmail.com>
2026-05-14 15:19:00 +0700

    Hoàn Thành Buoi-2

---
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using CMS.Data.Entities; // Kết nối tới lớp dữ liệu bạn vừa tạo
+using Microsoft.AspNetCore.Mvc;
+using CMS.Data;
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    // "Tiêm" kết nối vào Controller
+    public CategoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        // Lấy dữ liệu THẬT từ bảng Categories trong SQL
+        var data = _context.Categories.ToList();
+        return View(data);
+    }
+}
+
